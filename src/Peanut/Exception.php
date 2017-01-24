14ed31e9066dad0e2db2c8e9db4f6f669df809bb
yejune <kwon@yejune.com>
2017-01-24 14:53:16 +0900

    add exception class

---
+++ src/Peanut/Exception.php
@@ -0,0 +1,24 @@
+<?php
+namespace Peanut;
+
+class Exception extends \Exception
+{
+    public function __construct($e, $status = '500')
+    {
+        if (true === is_object($e)) {
+            $e = $e->getMessage().' in '.$e->getFile().' on line '.$e->getLine();
+        }
+        $di = \Phalcon\DI\FactoryDefault::getDefault();
+        if (null === $di) {
+            $di = new \Phalcon\DI\FactoryDefault;
+        }
+        $di->setShared('response', function () {
+            return new \Peanut\Phalcon\Http\Response();
+        });
+        $di->getShared('response')->content([
+            'status'  => $status,
+            'message' => $e,
+        ])->send();
+        die();
+    }
+}
