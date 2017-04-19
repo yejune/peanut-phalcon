d908b1660f6ecf0b6b2c41b627ed47fadf55dd40
yejune <kwon@yejune.com>
2017-04-19 11:48:25 +0900

    Set all not null fields to their default value.

--- src/Peanut/Phalcon/Mvc/Model.php
+++ src/Peanut/Phalcon/Mvc/Model.php
@@ -7,4 +7,16 @@ class Model extends \Phalcon\Mvc\Model
     {
         $this->_related[$alias][] = $properties;
     }
+    public function beforeSave()
+    {
+        $metaData   = $this->getModelsMetaData();
+        $attributes = $metaData->getNotNullAttributes($this);
+
+        // Set all not null fields to their default value.
+        foreach ($attributes as $field) {
+            if (false === isset($this->{$field}) || true === is_null($this->{$field})) {
+                $this->{$field} = new \Phalcon\Db\RawValue('default');
+            }
+        }
+    }
 }
