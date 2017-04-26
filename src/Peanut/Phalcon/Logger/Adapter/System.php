290360157fae1f4df4bb9908bc2aa1e896b605a2
yejune <kwon@yejune.com>
2017-04-26 12:12:41 +0900

    system logger cleanup

--- src/Peanut/Phalcon/Logger/Adapter/System.php
+++ src/Peanut/Phalcon/Logger/Adapter/System.php
@@ -3,32 +3,59 @@ namespace Peanut\Phalcon\Logger\Adapter;
 
 class System
 {
+    public $outputFormat = 'json';
+    public function printOutput($message, $type = 'log')
+    {
+        if (false === is_array($message)) {
+            $messages = [
+                'message' => $message,
+            ];
+        } else {
+            $messages = $message;
+        }
+        $array = [
+            'adapter'  => 'system',
+            'type'     => $type,
+            'messages' => $messages,
+            'time'     => date('Y-m-d\TH:i:sP'),
+        ];
+        if ('json' == $this->outputFormat) {
+            $format = json_encode($array);
+        } else {
+            $format = http_build_query($array);
+        }
+        error_log($format);
+    }
+    public function setOutputFormat($format)
+    {
+        $this->outputFormat = $format;
+    }
     public function debug($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
     public function info($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
     public function notice($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
     public function warning($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
     public function error($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
     public function critical($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
     public function log($message)
     {
-        error_log($message);
+        $this->printOutput($message, __METHOD__);
     }
 }
