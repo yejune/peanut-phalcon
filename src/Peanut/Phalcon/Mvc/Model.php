aa5f81cd3938bfba7c42dd55e8153dd1d80183ff
yejune <kwon@yejune.com>
2017-05-07 21:39:26 +0900

    add model create, update, save method

--- src/Peanut/Phalcon/Mvc/Model.php
+++ src/Peanut/Phalcon/Mvc/Model.php
@@ -3,11 +3,11 @@ namespace Peanut\Phalcon\Mvc;
 
 class Model extends \Phalcon\Mvc\Model
 {
-    public function setRelated($alias, $properties)
+    public function setRelated($alias, $properties) : void
     {
         $this->_related[$alias][] = $properties;
     }
-    public function beforeSave()
+    public function beforeSave() : void
     {
         $metaData   = $this->getModelsMetaData();
         $attributes = $metaData->getNotNullAttributes($this);
@@ -18,5 +18,38 @@ class Model extends \Phalcon\Mvc\Model
                 $this->{$field} = new \Phalcon\Db\RawValue('default');
             }
         }
+    }
+    public function save($data = null, $whiteList = null) : boolean
+    {
+        $result = parent::save($data, $whiteList);
+        if (false === $result || parent::getMessages()) {
+            foreach (parent::getMessages() as $message) {
+                throw new \Exception($message->getMessage());
+            }
+        }
+
+        return $result;
+    }
+    public function create($data = null, $whiteList = null) : boolean
+    {
+        $result = parent::create($data, $whiteList);
+        if (false === $result || parent::getMessages()) {
+            foreach (parent::getMessages() as $message) {
+                throw new \Exception($message->getMessage());
+            }
+        }
+
+        return $result;
+    }
+    public function update($data = null, $whiteList = null) : boolean
+    {
+        $result = parent::update($data, $whiteList);
+        if (false === $result || parent::getMessages()) {
+            foreach (parent::getMessages() as $message) {
+                throw new \Exception($message->getMessage());
+            }
+        }
+
+        return $result;
     }
 }
