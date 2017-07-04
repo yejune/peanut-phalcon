61cd30eac28607d3681c4930c5d6b3d7ffe2713a
yejune <kwon@yejune.com>
2017-07-04 13:38:36 +0900

    logger 정리

--- src/Peanut/Phalcon/Logger/Adapter/System.php
+++ src/Peanut/Phalcon/Logger/Adapter/System.php
@@ -4,6 +4,11 @@ namespace Peanut\Phalcon\Logger\Adapter;
 class System
 {
     public $outputFormat = 'json';
+    public $fifo;
+    public function __construct($fifo = null)
+    {
+        $this->fifo = $fifo;
+    }
     public function printOutput($message, $type = 'log')
     {
         if (false === is_array($message)) {
@@ -14,17 +19,25 @@ class System
             $messages = $message;
         }
         $array = [
-            'adapter'  => 'system',
-            'type'     => $type,
-            'messages' => $messages,
-            'time'     => date('Y-m-d\TH:i:sP'),
+            'type'        => 'php',
+            'mode'        => $type,
+            'fields'      => $messages,
+            'time'        => date('Y-m-d\TH:i:sP'),
+            'remote_addr' => $this->getClientIp(),
+            'user_agent'  => $_SERVER['HTTP_USER_AGENT']??'',
+            'request_uri' => $_SERVER['PATH_INFO']??'',
+            'host'        => $_SERVER['HTTP_HOST']??''
         ];
         if ('json' == $this->outputFormat) {
             $format = json_encode($array);
         } else {
             $format = http_build_query($array);
         }
-        error_log($format.PHP_EOL);
+        if ($this->fifo) {
+            error_log($format.PHP_EOL, 3, $this->fifo);
+        } else {
+            error_log($format.PHP_EOL);
+        }
     }
     public function setOutputFormat($format)
     {
@@ -57,5 +70,24 @@ class System
     public function log($message)
     {
         $this->printOutput($message, __FUNCTION__);
+    }
+    public function getClientIp()
+    {
+        $ipaddress = '';
+        if (true === isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (true === isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (true === isset($_SERVER['HTTP_X_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        } elseif (true === isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        } elseif (true === isset($_SERVER['HTTP_FORWARDED'])) {
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        } elseif (true === isset($_SERVER['REMOTE_ADDR'])) {
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ipaddress;
     }
 }
