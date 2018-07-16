3174ebe0d277cb0b8fad0eb372291eeff8866f3b
yejune <k@yejune.com>
2018-07-16 23:54:30 +0900

    delete test code

--- src/Peanut/Schema/Fields/SelectField.php
+++ src/Peanut/Schema/Fields/SelectField.php
@@ -11,7 +11,7 @@ class SelectField extends \Peanut\Schema\Fields
         $id       = $this->getId();
         $required = $this->getRequired();
         $readonly = $this->getReadonly();
-        pr($this);
+
         // true일때는 무조건 readonly
         // exist일때는 값이 있을 경우에만
         if ($readonly == 'exist' && !$value) {
