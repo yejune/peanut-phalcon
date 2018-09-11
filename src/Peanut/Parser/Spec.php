76127721deb5e2661d817121a392e02822c37c2f
yejune <k@yejune.com>
2018-09-12 04:02:32 +0900

    yml, yml file에 추가로 array 형식 지원 in parser spec

--- src/Peanut/Parser/Spec.php
+++ src/Peanut/Parser/Spec.php
@@ -1,36 +1,47 @@
 <?php
+
 namespace Peanut\Parser;
 
 class Spec
 {
     public static $baseUrl = '';
+
     public static function parse($path, $mix = [], $mix2 = [])
     {
-        if (true === file_exists($path)) {
-            $yml = file_get_contents($path);
+        if (true === \is_array($path)) {
+            $arr = $path;
         } else {
-            $yml = $path;
-        }
-        if (!static::$baseUrl) {
-            static::$baseUrl = dirname($path).'/';
-        }
-        $arr = yaml_parse($yml);
+            if (true === \file_exists($path)) {
+                $yml = \file_get_contents($path);
+            } else {
+                $yml = $path;
+            }
 
-        $arr = static::_parse($arr);
-        if ($mix) {
-            $arr = static::mix($arr, $mix);
-        }
-        if ($mix2) {
-            $arr = static::mix($arr, $mix2);
+            if (!static::$baseUrl) {
+                static::$baseUrl = \dirname($path) . '/';
+            }
+            $arr = \yaml_parse($yml);
+
+            $arr = static::_parse($arr);
+
+            if ($mix) {
+                $arr = static::mix($arr, $mix);
+            }
+
+            if ($mix2) {
+                $arr = static::mix($arr, $mix2);
+            }
         }
 
         return $arr;
     }
+
     public static function mix(array $array1 = [], array $array2 = [])
     {
         $merged = $array1;
+
         foreach ($array2 as $key => &$value) {
-            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+            if (\is_array($value) && isset($merged[$key]) && \is_array($merged[$key])) {
                 $merged[$key] = static::mix($merged[$key], $value);
             } else {
                 $merged[$key] = $value;
@@ -39,28 +50,32 @@ class Spec
 
         return $merged;
     }
+
     private static function _parse($arr = []) : array
     {
         $return = [];
+
         foreach ($arr as $key => $value) {
-            if ($key === '$ref') {
-                if (false === is_array($value)) {
+            if ('$ref' === $key) {
+                if (false === \is_array($value)) {
                     $value = [$value];
                 }
                 $data = [];
+
                 foreach ($value as $path) {
                     if (0 !== \strpos($path, '/')) {
-                        $path =  static::$baseUrl.$path.'';
+                        $path = static::$baseUrl . $path . '';
                     }
-                    $yml  = static::parse($path);
+                    $yml = static::parse($path);
+
                     if (true === isset($yml['properties'])) {
-                        $data = array_merge($data, $yml['properties']);
+                        $data = \array_merge($data, $yml['properties']);
                     }
                 }
                 $yml = static::_parse($data);
 
-                $return = array_merge($return, $yml);
-            } elseif (true === is_array($value)) {
+                $return = \array_merge($return, $yml);
+            } elseif (true === \is_array($value)) {
                 $return[$key] = static::_parse($value);
             } else {
                 $return[$key] = $value;
